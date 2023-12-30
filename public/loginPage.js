9839fd5a02128978a10f710b925ddf18dc85b3d4
Yura <bobr515yura@yandex.ru>
2023-12-30 17:18:29 +0300

    Mytask

---
+++ public/loginPage.js
@@ -0,0 +1,31 @@
+"use strict";
+const userForm = new UserForm();
+userForm.loginFormCallback=(data)=> {
+    console.log(data);
+    ApiConnector.login(data, (response)=>{
+        console.log(response);
+        if (response) {
+            location.reload();
+        } else {
+            let error=new Error("ошибка");
+            throw error;
+        }
+        
+
+    });
+
+}
+userForm.registerFormCallback=(data)=> {
+    console.log(data);
+    ApiConnector.register(data, (response)=>{
+        console.log(response);
+        if (response) {
+            location.reload();
+        } else {
+            let error=new Error("ошибка");
+            throw error;
+        }
+        
+
+    });
+}
